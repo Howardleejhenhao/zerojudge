d4ff02bd82984f73edc14457047d1640b07f36e6
howard <howard20051109@gmail.com>
2022-10-30 15:22:11 +0800

    1030

--- c147/main.cpp
+++ c147/main.cpp
@@ -16,9 +16,32 @@
 
 using namespace std;
 
+// 01背包
+#define N 1000100
+int dp[N];
+
 void solve()
 {
-    
+    string s1, s2;
+    stringstream ss;
+    getline(cin, s1);
+    getline(cin, s2);
+    int n;
+    vector<int> a, w;
+    cin >> n;
+    ss << s1;
+    int now;
+    while(ss >> now) a.push_back(now);
+    ss.clear();
+    ss << s2;
+    while(ss >> now) w.push_back(now);
+    // 要注意枚舉的方向，某個位置代表用到這個至少要花多少，若用一維陣列DP的話，要注意枚舉的方向
+    for(int j = 0; j < w.size(); j++) {
+        for(int i = n; i >= 0; i--) {
+            if(i - a[j] >= 0) dp[i] = max(dp[i], dp[i - a[j]] + w[j]);
+        }
+    }
+    cout << dp[n] << '\n';
     return;
 }
 
