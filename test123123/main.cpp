53d489cd34b1fe7d171500013add42ca22f6e2b0
howard <howard20051109@gmail.com>
2022-12-20 23:12:36 +0800

    Update main.cpp

--- test123123/main.cpp
+++ test123123/main.cpp
@@ -1,15 +1,13 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int tt;
-    cin >> tt;
-    while(tt--){
-        int n[3];
-        cin >> n[0] >> n[1] >> n[2];
-        sort(n, n + 3);
-        cout << n[1] << '\n';
+int main()
+{
+    int n = 10;
+    int a[n], b[n];
+    for(int i = 0; i < n; i++) {
+        if(a[i] == b[i]) {
+
+        }
     }
 }
