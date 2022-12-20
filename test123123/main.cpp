13a8c449899dbed5d15afe27f90e32c93fa0ef59
howard <howard20051109@gmail.com>
2022-12-20 22:38:48 +0800

    1220

--- test123123/main.cpp
+++ test123123/main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main(){
-    cout << fixed << 1.000000 << '\n';
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tt;
+    cin >> tt;
+    while(tt--){
+        int n[3];
+        cin >> n[0] >> n[1] >> n[2];
+        sort(n, n + 3);
+        cout << n[1] << '\n';
+    }
 }
